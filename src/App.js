0b7ba35d63a4943e312ad03148f8c06579faa63e
Бек Аскер <2004bek.asker@mail.ru>
2025-01-18 20:46:30 +0500

    feat: Add select to choose question

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from "react";
-import QuestionCard from "./components/QuestionCard/QuestionCard";
-import Streak from "./components/Streak/Streak";
+import React, { useState } from "react"
+import QuestionCard from "./components/QuestionCard/QuestionCard"
+import Streak from "./components/Streak/Streak"
 
 function App() {
+
+  const [selectedValue, setSelectedValue] = useState(1)
+
   return (
     <div className="App">
-      {/* <Streak streak={}/> */}
-      <QuestionCard />
+      <select
+        value={selectedValue}
+        onChange={(e) => setSelectedValue(Number(e.target.value))}
+      >
+        {Array.from({ length: 180 }, (_, i) => (
+          <option key={i} value={i + 1}>
+            {i + 1}
+          </option>
+        ))}
+      </select>
+      <QuestionCard selectedOption={selectedValue}/>
     </div>
-  );
+  )
 }
 
 export default App
